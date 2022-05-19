6d1fca89ecb4631a77d3fa94d225b4b033dee7fc
YJ <crona.yc@gmail.com>
2022-05-19 12:14:45 +0900

    스타일시트 적용

--- day05/src/App.js
+++ day05/src/App.js
@@ -1,20 +1,54 @@
 import React, { useState } from 'react';
+import styled,{css} from 'styled-components';
 import './App.css';
+import CheckBox from './component/checkbox';
 // import Box01 from './component/box';
 // import Box02 from './component/box02';
-import CheckBox from './component/checkbox';
+
+// [모듈설치]
+// yarn add stryled-components 상단에 import styled from 'styled-components';
+// yarn add react-icons
+// https://react-icons.github.io/react-icons/#/
+
+// 선언형 컴포넌트
+const TestBox = styled.div`
+  width:300px;
+  height:300px;
+  background:skyblue;
+`;
+
+const Circle = styled.div`
+  width:300px;
+  height:300px;
+  border-radius:50%;
+  background:${props => (props.color || 'black')};
+  ${props => (props.big && css`
+  width:150px;
+  height:150px;`)}
+`;
+
+
 
 function App() {
   const [check, setCheck] = useState(false);
   const onChange = e => {
     setCheck(e.target.checked);
+    setBox(e.target.box1);
   };
+  
+
+  const [box, setBox] = useState(true);
+  
   return (
     <div>
       {/* <div></div> 스타일시트 적용됨 <></> 스타일시트적용안됨 */}
       {/* <Box01 />
       <Box02 /> */}
-      <CheckBox onChange={onChange} checked={check}>약관에 모두 동의</CheckBox>
+      <CheckBox onChange={onChange} checked={check} box={box}>약관에 모두 동의</CheckBox>
+      <TestBox/>
+      <Circle color='deeppink'/>
+      <Circle/>
+      <Circle big/>
     </div>
   );
 }
--- day05/src/component/checkbox.jsx
+++ day05/src/component/checkbox.jsx
@@ -1,16 +1,21 @@
 import React from "react";
 import styles from './checkbox.module.css';
 import { MdCheckBox, MdCheckBoxOutlineBlank} from 'react-icons/md';
+import { DiAndroid } from "react-icons/di";
 
-function CheckBox ({children, checked}) {
+function CheckBox ({children, checked, box, ...rest}) {
     return (
-        <div>
+        <div className={styles.checkbox}>
             <label>
-                <input type="checkbox" checked={checked} /><span>{children}</span>
+                <input type="checkbox" checked={checked} {...rest} />
                 <div className={styles.icon}>
                     {checked ? (<MdCheckBox className={styles.checked}/>): (<MdCheckBoxOutlineBlank/>)}
                 </div>
             </label>
+            <span>{children}</span>
+            <div>
+                {box ? (<DiAndroid className={styles.icon2}/>) : (<DiAndroid className={styles.icon2_1}/>)}
+            </div>
         </div>
     )
 }
